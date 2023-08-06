3950a4a0110b630b6d7b8a8ef38dedfb24ea41b7
mahmudhasankhan <khanzadeh008@gmail.com>
2023-08-06 15:25:28 +0600

    minor changes done

--- chain.py
+++ chain.py
@@ -10,8 +10,76 @@ from langchain.memory import ConversationBufferWindowMemory
 
 from dotenv import load_dotenv, find_dotenv
 
+from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.callbacks.tracers import LangChainTracer
+
 
 _ = load_dotenv(find_dotenv())
+
+
+def callback_chain(
+        vectorstore: Pinecone,
+        question_handler,
+        stream_handler
+) -> ConversationalRetrievalChain:
+
+    model = ChatOpenAI(streaming=True,
+                       temperature=0.0,
+                       callbacks=[question_handler, stream_handler])
+
+    template = """You are a helpful AI assistant that answers questions about
+    an e-commerce company called "Sindabad.com" in a friendly and polite
+    manner. You will be given a "Context" that will represent Sindabad.com's
+    product inventory. Users might ask about products, they might want to
+    know your suggestions as well. Most importantly, they might ask about
+    specific product and its associated product link. If they want to know
+    about product links, you will provide it accordingly with the help of the
+    given "Context". It should be noted, only the Mobile Phones sub category
+    has product links in them,therefore, should the users ask a different
+    product and want to know their product links you should tell them, "Only
+    the mobile phones section has product links at the moment, in future
+    additional sections will have product links in them". Answer the question
+    in your own words as truthfully as possible from the context given to you.
+    If you do not know the answer to the question, simply respond with
+    "I don't know. Could you please rephrase the question?". If questions
+    are asked where there is no relevant information available in the context,
+    answer the question with your existing knowledge on that question and
+    "ignore" the "Context" given to you.
+
+    Context:\n {context}
+
+    Human: {question}
+    Assistant:"""
+
+    prompt = PromptTemplate(
+        input_variables=["context", "question"],
+        template=template
+    )
+    condense_prompt = PromptTemplate(
+        input_variables=["question", "chat_history"],
+        template="""Combine the chat history and follow up question into
+        a standalone question.
+        Chat History: {chat_history}
+        Follow up question: {question}
+        """
+    )
+
+    memory = ConversationBufferWindowMemory(
+        input_key="question",
+        memory_key="chat_history",
+        k=3,
+        return_messages=True,
+        output_key="answer")
+
+    return ConversationalRetrievalChain.from_llm(
+        model,
+        retriever=vectorstore.as_retriever(),
+        return_source_documents=True,
+        condense_question_prompt=condense_prompt,
+        combine_docs_chain_kwargs={'prompt': prompt},
+        verbose=False,
+        memory=memory,
+        callbacks=[AsyncCallbackHandler()])
 
 
 def make_chain(pinecone_index: str) -> ConversationalRetrievalChain:
@@ -56,16 +124,16 @@ def make_chain(pinecone_index: str) -> ConversationalRetrievalChain:
         Follow up question: {question}
         """
     )
-    memory = ConversationBufferWindowMemory(input_key="question",
-                                            memory_key="chat_history",
-                                            k=3,
-                                            return_messages=True,
-                                            output_key="answer")
-    return ConversationalRetrievalChain.from_llm(model,
-                                                 retriever=vector_store.as_retriever(),
-                                                 return_source_documents=True,
-                                                 condense_question_prompt=condense_prompt,
-                                                 combine_docs_chain_kwargs={
-                                                     'prompt': prompt},
-                                                 memory=memory,
-                                                 verbose=False)
+    # memory = ConversationBufferWindowMemory(
+    #     input_key="question",
+    #     memory_key="chat_history",
+    #     k=3,
+    #     return_messages=True,
+    #     output_key="answer")
+    return ConversationalRetrievalChain.from_llm(
+        model,
+        retriever=vector_store.as_retriever(),
+        return_source_documents=True,
+        condense_question_prompt=condense_prompt,
+        combine_docs_chain_kwargs={'prompt': prompt},
+        verbose=False)
